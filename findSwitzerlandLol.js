c05df7bf3bf40726052f3aad8c7950c93ccf10e2
craboudja <mon.adresse.de.zebre@gmail.com>
2022-01-15 23:47:54 +0100

    findSwitzerland ok

--- findSwitzerlandLol.js
+++ findSwitzerlandLol.js
@@ -9,3 +9,14 @@
 
 // $> ruby exo.rb 2 2 2
 // erreur.
+
+const arguments = process.argv
+
+let chiffres = [arguments[2], arguments[3], arguments[4]]
+const unique = [...new Set(chiffres)]
+if (arguments.length === 5 && unique.length === 3) {
+    chiffres = chiffres.sort()
+    console.log(chiffres[1])
+} else {
+    console.log('Erreur, ma poule! Essaie encore')
+}
