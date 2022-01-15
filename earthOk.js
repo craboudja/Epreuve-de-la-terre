ac95cdf14ee8d3d3c7a3855e86df67a6b47aab8c
craboudja <mon.adresse.de.zebre@gmail.com>
2022-01-15 17:40:56 +0100

    first commit

--- earthOk.js
+++ earthOk.js
@@ -6,3 +6,5 @@
 // $>
 
 // Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+console.log("J'ai terminé l'Epreuve de la Terre et c'était pas fini en fait parce que je l'ai fait dans l'ordre alphabétique mais jusque là, c'est amusant.")
