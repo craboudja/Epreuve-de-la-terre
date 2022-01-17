0e12a4b649e5a213d0ef755e4e4a8c2774bb9a11
craboudja <mon.adresse.de.zebre@gmail.com>
2022-01-17 22:02:49 +0100

    auto commit

--- numberExponent.js
+++ numberExponent.js
@@ -7,3 +7,13 @@
 // 8
 
 // Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
+
+const arguments = process.argv
+const num1 = arguments[2]
+const num2 = arguments[3] 
+
+if (!isNaN(num1) && !isNaN(num2) && arguments.length == 4) {
+    console.log(num1 ** num2)
+} else {
+    console.log("erreur")
+}
